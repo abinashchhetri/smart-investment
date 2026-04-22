8028abff7cd9637f0a4dc151c57de6227bcbb830
abinashchhetri <abinashchhetri.work@gmail.com>
2026-04-23 03:31:02 +0545

    implemented the header

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="min-h-screen text-green-400">
+      <Header />
+      <div className="container py-10">
+        {/* header */}
+        {children}
+      </div>
+    </main>
+  );
+};
+
+export default layout;
---
+++ components/NavItems.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { NAV_ITEMS } from "@/lib/constants";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavItems = () => {
+  const pathname = usePathname();
+
+  const isActive = (href: string) => pathname === href;
+
+  return (
+    <ul className="flex flex-col sm:flex-row p-2 gap-3 sm:gap-10 font-medium">
+      {NAV_ITEMS.map(({ href, title }) => (
+        <li key={href}>
+          <Link
+            href={href}
+            className={`hover:text-yellow-500 transition-colors ${
+              isActive(href) ? "text-green-100" : ""
+            }`}
+          >
+            {title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavItems;
