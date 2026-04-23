b6e723279c017f8a67dd2f05a3bfb7a56b2ff8ba
abinashchhetri <abinashchhetri.work@gmail.com>
2026-04-23 13:55:13 +0545

    feat(ui): added the ui for the auth login and signup

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="min-h-screen text-green-400">
+    <main className="min-h-screen text-gray-400">
       <Header />
       <div className="container py-10">
         {/* header */}
