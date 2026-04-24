964ac952d7282ee92d5dc5d80e5c000d6788844e
abinashchhetri <abinashchhetri.work@gmail.com>
2026-04-24 22:22:33 +0545

    added the authentication flow

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -7,8 +7,12 @@ import SelectField from "@/components/forms/SelectField";
 import {INVESTMENT_GOALS, PREFERRED_INDUSTRIES, RISK_TOLERANCE_OPTIONS} from "@/lib/constants";
 import {CountrySelectField} from "@/components/forms/CountrySelectField";
 import FooterLink from "@/components/forms/FooterLink";
+import {signUpWithEmail} from "@/lib/actions/auth.actions";
+import {useRouter} from "next/navigation";
+import {toast} from "sonner";
 
 const SignUp = () => {
+     const router = useRouter()
     const {
         register,
         handleSubmit,
@@ -29,9 +33,13 @@ const SignUp = () => {
 
     const onSubmit = async (data: SignUpFormData) => {
         try {
-            console.log(data);
+             const result = await signUpWithEmail(data);
+            if(result.success) router.push('/');
         } catch (e) {
             console.error(e);
+            toast.error('Sign up failed', {
+                description: e instanceof Error ? e.message : 'Failed to create an account.'
+            })
         }
     }
 
@@ -53,12 +61,13 @@ const SignUp = () => {
                     name="email"
                     label="Email"
                     placeholder="contact@jsmastery.com"
+                    type="email"
                     register={register}
                     error={errors.email}
                     validation={{
                         required: 'Email name is required',
                         pattern: {
-                            value: /^\w+@\w+\.\w+$/,
+                            value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
                             message: 'Email address is invalid'
                         }
                     }}
